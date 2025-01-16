3ef0bc606f82d378c26d3dab3e070d1a6c60b7b0
L33117 <L33117@lab.aca>
2025-01-16 11:17:05 -0500

    correciones ej2

--- ej2.ts
+++ ej2.ts
@@ -1,18 +1,18 @@
-type Punto = [number, number];
+export type Punto = [number, number];
 
 const p1: Punto = [1, 2];
 const p2: Punto = [4, 6];
 
 type TipoDEP = (p1: Punto, p2: Punto) => number;
 
-const distanciaEntrePuntos: TipoDEP = (p1, p2) => {
+export const distanciaEntrePuntos: TipoDEP = (p1, p2) => {
     return Math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2);
 }
 
 type TipoMP = (p: Punto, [dx, dy]: [number, number]) => Punto;
 
-const moverPunto: TipoMP = (p, tupla) => {
-    p[0] + tupla[0];
-    p[1] + tupla[1];
+export const moverPunto: TipoMP = (p, tupla) => {
+    p[0] += tupla[0];
+    p[1] += tupla[1];
     return p;
 }
--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
 import { calcularPromedio, filtrarNumerosMayores } from "./ej1";
+import { distanciaEntrePuntos, moverPunto, Punto } from "./ej2";
 
 const main = () => {
     const numeros: number[] = [5, 8, 3, 10, 2, 7];
-    const p1: number[] = filtrarNumerosMayores(numeros, 5);
-    console.log(p1);
+    const lp: number[] = filtrarNumerosMayores(numeros, 5);
+    console.log(lp);
 
-    const p2: number = calcularPromedio(numeros);
-    console.log(p2);
+    const x: number = calcularPromedio(numeros);
+    console.log(x);
+
+    const p1: Punto = [1, 2];
+    const p2: Punto = [4, 6];
+    const distancia = distanciaEntrePuntos(p1, p2); 
+    console.log(distancia);
+
+    const nuevoPunto = moverPunto(p1, [3, 4]);
+    console.log(nuevoPunto);
 }
 
 main()
